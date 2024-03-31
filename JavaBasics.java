ae76cf000ce768146cd2e5006ac6003629e998a4
Puja <pujaranidalei456@gmail.com>
2024-04-01 00:37:02 +0530

    first commit

---
+++ JavaBasics.java
@@ -0,0 +1,19 @@
+
+// public class JavaBasics{
+//     public static void main(String args[]){
+//     char a = 'a';
+//     char b = 'b';
+//     System.out.println((int)(b));
+//     System.out.println(b-a);
+//  }
+// }
+
+
+
+public class JavaBasics{
+   public static void main(String args[]){
+    System.out.print("Hello World");
+   }
+}
+
+
